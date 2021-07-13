63f60a1263852a03abb26cba0b29f34ec8f72d73
mauricio <mauricio2017rubiale@gmail.com>
2021-07-13 16:20:22 -0300

    teste

--- app.js
+++ app.js
@@ -37,8 +37,19 @@ app.get('/', (req, res) => {
 const client = new Client({
   restartOnAuthFail: true,
   puppeteer: {
-    browserWSEndpoint: `ws://localhost:3000`
+    headless: true,
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--disable-dev-shm-usage',
+      '--disable-accelerated-2d-canvas',
+      '--no-first-run',
+      '--no-zygote',
+      '--single-process', // <- this one doesn't works in Windows
+      '--disable-gpu'
+    ],
   },
+  session: sessionCfg
 });
 
 client.on('message', msg => {
@@ -52,7 +63,7 @@ client.on('message', msg => {
     };
     
     console.log("foi: " + JSON.stringify(msg))
-
+    
    axios.post("https://oxigenio.sharkdata.com.br/api/WhatsWeb/Receberheruku", config)
    //axios.post("https://0e6aa7d50090.ngrok.io/api/WhatsWeb/Receberheruku", config)
         .then(function (response) {
