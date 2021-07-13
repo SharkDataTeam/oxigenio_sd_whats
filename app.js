8b7f9e62bd6cc44fcfaa1fe0c9bc1f760908d634
mauricio <mauricio2017rubiale@gmail.com>
2021-07-13 15:16:35 -0300

    espaco a mais

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ client.on('message', msg => {
     };
     
     console.log("foi: " + JSON.stringify(msg))
+    
    axios.post("https://oxigenio.sharkdata.com.br/api/WhatsWeb/Receberheruku", config)
    //axios.post("https://0e6aa7d50090.ngrok.io/api/WhatsWeb/Receberheruku", config)
         .then(function (response) {
