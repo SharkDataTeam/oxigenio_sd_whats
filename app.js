b93e456dfe0b28bdb85946d04eba3e5238f0b46c
mauricio <mauricio2017rubiale@gmail.com>
2021-07-13 16:33:34 -0300

    teste

--- app.js
+++ app.js
@@ -34,23 +34,7 @@ app.get('/', (req, res) => {
   });
 });
 
-const client = new Client({
-  restartOnAuthFail: true,
-  puppeteer: {
-    headless: true,
-    args: [
-      '--no-sandbox',
-      '--disable-setuid-sandbox',
-      '--disable-dev-shm-usage',
-      '--disable-accelerated-2d-canvas',
-      '--no-first-run',
-      '--no-zygote',
-      '--single-process', // <- this one doesn't works in Windows
-      '--disable-gpu'
-    ],
-  },
-  session: sessionCfg
-});
+const client = new Client();
 
 client.on('message', msg => {
     console.log("foi: entrou ")
